e5d3390759f84e40f5a67b3416004484fb0dc098
jgoh88 <jarrodgoh88@hotmail.com>
2023-06-30 14:27:39 +0800

    set basic structure

---
+++ art-shop/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ backend/index.js
@@ -0,0 +1,23 @@
+// import modules
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+
+// import controllers
+
+
+const PORT = process.env.PORT
+const server = express()
+mongoose.connect(process.env.MONGODB)
+    .then(() => console.log('Connected to mongdb'))
+
+server.use(cors())
+server.use(express.json())
+server.use(morgan('dev'))
+
+// use controllers
+
+
+server.listen(PORT, () => console.log(`Running on port ${PORT}`))
