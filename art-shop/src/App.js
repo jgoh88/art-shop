95d8868d9a4c7fcb26e8930b67f07fc8c27640a1
jgoh88 <jarrodgoh88@hotmail.com>
2023-07-03 14:36:10 +0800

    completed useUserHook for frontend

--- art-shop/src/App.js
+++ art-shop/src/App.js
@@ -3,17 +3,20 @@ import NavBar from './components/NavBar';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Home from "./components/Home";
+import { UserProvider } from './hooks/useUserHook';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path={'/signup'} element={<Signup />} />
-        <Route path={'/login'} element={<Login />} />
-        <Route path={"/"} element={<Home />} />
-      </Routes>
+      <UserProvider>
+        <NavBar />
+        <Routes>
+          <Route path={'/signup'} element={<Signup />} />
+          <Route path={'/login'} element={<Login />} />
+          <Route path={"/"} element={<Home />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }
