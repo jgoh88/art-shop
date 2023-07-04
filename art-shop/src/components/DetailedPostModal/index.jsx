c2fe0bc9e6bd25a3c08dce249abce0652d4cdfaf
mohamadhazem <mohamadhazemt@gmail.com>
2023-07-04 23:35:06 +0800

    pass Modal props

---
+++ art-shop/src/components/DetailedPostModal/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import {Col, Row, Container, Card} from "react-bootstrap";
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
+
+
+function DetailedPostModalComponent (props) {
+    
+    const [modal, setModal] = useState(false);
+    const toggle = ()=> setModal(!modal);
+  
+    return (
+     <div>
+        <Button onClick={toggle}>
+          Details
+        </Button>
+        <Modal isOpen={modal} toggle={toggle} {...props}>
+          <ModalHeader toggle={toggle}><h1>Details</h1></ModalHeader>
+          <ModalBody>
+           <Container>
+           <Row className="mt-3 text-center">
+                <Col>
+                {props.name}
+                </Col>
+            </Row>
+            <Row>
+ <Col>
+   <Card.Img variant="top" src={props.img || "https://www.thesprucepets.com/thmb/j86Zss9kZEIXa54FcOQaR7eCmfY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg"} />
+       <Col>
+         <div>
+           <div className={`fw-bold`}>RM {props.price}</div>
+         </div>
+         <div>{props.name}</div>
+       </Col>
+       </Col>
+</Row>
+            </Container>
+          </ModalBody>
+          <ModalFooter>
+
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+
+  export default DetailedPostModalComponent
