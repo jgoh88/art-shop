cb771cee365221a7ecdac9274ddd5c6453026e7a
jgoh88 <jarrodgoh88@hotmail.com>
2023-07-05 17:45:36 +0800

    completed masonry layout for home and search

--- art-shop/src/components/Artworks/index.jsx
+++ art-shop/src/components/Artworks/index.jsx
@@ -2,16 +2,17 @@ import {Col, Row, Card, Button } from "react-bootstrap";
 import DetailedPostModalComponent from "../DetailedPostModal";
 import { useCartHook } from "../../hooks/useCartHook";
 import { useUserHook } from "../../hooks/useUserHook";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function Artworks({ artworks }) {
     const userHook = useUserHook()
     const cartHook = useCartHook()
 
     return (
-        <>
+        <Masonry columnsCount={4} gutter="15px">
             {artworks.map((artwork) => {
             return(
-                <Col md={3} className={"mb-3"} key={artwork._id}>
+                <Col md={3} key={artwork._id} style={{ width: "100%" }}>
                     <Card>
                     <Card.Img variant="top" src={artwork.img || "https://www.thesprucepets.com/thmb/j86Zss9kZEIXa54FcOQaR7eCmfY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg"} />
                     <Card.Body>
@@ -40,6 +41,6 @@ export default function Artworks({ artworks }) {
                     </Card>
                 </Col> 
             )})}
-        </>
+        </Masonry>
     )
 }
