990e6f6f30869f6aeace9fcc7fceab3f3238070b
jgoh88 <jarrodgoh88@hotmail.com>
2023-07-07 17:53:59 +0800

    save

--- art-shop/src/components/About/index.jsx
+++ art-shop/src/components/About/index.jsx
@@ -9,11 +9,16 @@ export default function About() {
                 </Col>
             </Row>
             <Row className="mt-4 d-flex justify-content-center">
-                <Col xs={3} className="text-center fw-medium" style={{width: "80%"}}>
-                    <p className="">ArtShop is a platform for connecting aspiring and experienced artists with art enthusiasts and collectors. Artists can publish arts and details from their work in a polished, professional-looking online gallery, to promote their artwork.</p>
-                    <p></p>
+                <Col xs={7} className="text-justify fw-medium">
+                    <p>ArtShop is a platform for connecting aspiring and experienced artists with art enthusiasts and collectors. Using our platform, artists can publish arts and details from their work in a polished, professional-looking online gallery, to promote their artwork.</p>
+                    <p>Our mission is to help grow the art community and how gives us the ability to express ourselves. Through that expression, artists communicate by drawing on their own unique emotions, thoughts and experiences. These artworks can then be enjoyed by other as they see the world through the artists' eyes.</p>
                 </Col>
-            </Row>  
+            </Row>
+            <Row className="mt-4">
+                <Col className="d-flex justify-content-center">
+                    <img src="https://res.cloudinary.com/douu5ba5m/image/upload/v1688723290/GAProject3ArtShop/anpq0jbm3p2b79dn0dqe.webp" alt="Looking at art"/>
+                </Col>
+            </Row>
       </Container>
     )
 }
