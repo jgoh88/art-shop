0e7436d04cb92a059efcf73c871e8f0dcf51d032
jgoh88 <jarrodgoh88@hotmail.com>
2023-07-24 11:43:55 +0800

    save

--- art-shop/src/components/Artworks/index.jsx
+++ art-shop/src/components/Artworks/index.jsx
@@ -9,33 +9,35 @@ export default function Artworks({ artworks }) {
     const cartHook = useCartHook()
 
     return (
-        <Masonry columnsCount={4} gutter="15px">
-            {artworks.map((artwork) => {
-            return(
-                <Col md={3} key={artwork._id} style={{ width: "100%" }}>
-                    <Card>
-                    <Card.Img variant="top" src={artwork.img || "https://www.thesprucepets.com/thmb/j86Zss9kZEIXa54FcOQaR7eCmfY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg"} />
-                    <Card.Body>
-                        <Row>
-                        <Col>
-                            <div>
-                            <div className={`fw-bold`}>RM {artwork.price}</div>
-                            </div>
-                            <div>{artwork.name}</div>
-                        </Col>
-                        <Col>
-                            <ArtworksDetailsModal artwork={artwork} />
-                            {artwork.quantity > 0 
-                            ? !userHook.user 
-                                ? ''
-                                : <Button onClick={() => cartHook.addArtToCart(artwork._id.toString())}>Add to cart</Button>  
-                            : <div>No stock</div>}
-                        </Col>
-                        </Row>
-                    </Card.Body>
-                    </Card>
-                </Col> 
-            )})}
-        </Masonry>
+        <ResponsiveMasonry
+            columnsCountBreakPoints={{576: 1, 768: 2, 992: 3, 1200:4}}
+        >
+            <Masonry gutter="15px">
+                {artworks.map((artwork) => {
+                return(
+                    <Col md={3} key={artwork._id} style={{ width: "100%" }}>
+                        <Card>
+                        <Card.Img variant="top" src={artwork.img || "https://www.thesprucepets.com/thmb/j86Zss9kZEIXa54FcOQaR7eCmfY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg"} />
+                        <Card.Body>
+                            <Row>
+                            <Col>
+                                <div>{artwork.name}</div>
+                                <div className={`fw-bold`}>RM {artwork.price}</div>
+                            </Col>
+                            <Col>
+                                <ArtworksDetailsModal artwork={artwork} />
+                                {artwork.quantity > 0 
+                                ? !userHook.user 
+                                    ? ''
+                                    : <Button className="mt-1" onClick={() => cartHook.addArtToCart(artwork._id.toString())}>Add to cart</Button>  
+                                : <div>No stock</div>}
+                            </Col>
+                            </Row>
+                        </Card.Body>
+                        </Card>
+                    </Col> 
+                )})}
+            </Masonry>
+        </ResponsiveMasonry>
     )
 }
--- art-shop/src/components/EditProfile/index.jsx
+++ art-shop/src/components/EditProfile/index.jsx
@@ -11,70 +11,65 @@ function EditProfileComponent (props) {
     const [modal, setModal] = useState(false);
     const toggle = ()=> setModal(!modal);
 
-const navigate = useNavigate()
+    const navigate = useNavigate()
     const userHook = useUserHook()
     const [formInput, setFormInput] = useState({
-        username: '',
-        fullName: '',
-        email: '',
-        contactNo: ''
+        // username: props.username,
+        fullName: props.fullName,
+        email: props.email,
+        contactNo: props.contactNo,
+        address: props.address,
+        profilePic: props.profilePic,
     })
     const [errorMessage, setErrorMessage] = useState('')
     const cloudinaryRef = useRef()
     const widgetRef = useRef()
 
     useEffect(() => {
-      if (userHook.user) {
-          navigate('/profile')
-      }
-  }, [userHook.user])
-
-  useEffect(() => {
-    cloudinaryRef.current = window.cloudinary
-    widgetRef.current = cloudinaryRef.current.createUploadWidget({
-        cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-        uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
-    }, (err, result) => {
-        if (result.event === 'success') {
-            setFormInput((prevState) => ({...prevState, profilePic: result.info.secure_url,}))
-        }   
-    })
-}, [])
+        cloudinaryRef.current = window.cloudinary
+        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+            cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+            uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
+        }, (err, result) => {
+            if (result.event === 'success') {
+                setFormInput((prevState) => ({...prevState, profilePic: result.info.secure_url,}))
+            }   
+        })
+    }, [])
 
 
-function onFormChangeHandler(e) {
-  let value
-  if (e.target.name === 'seller') {
-      value = !!e.target.checked
-  } else {
-      value = e.target.value
-  }
-  setFormInput((prevState) => ({...prevState, [e.target.name]: value,}))
-}
+    function onFormChangeHandler(e) {
+        let value
+        if (e.target.name === 'seller') {
+            value = !!e.target.checked
+        } else {
+            value = e.target.value
+        }
+        setFormInput((prevState) => ({...prevState, [e.target.name]: value,}))
+    }
 
-async function onFormSubmitHandler(e) {
-  e.preventDefault()
-  setErrorMessage('')
-  try {
-      const res = await artShopBackendAxios.put('/profile', {data: formInput ,id: props.id,}, 
-      {
-        headers: {
-            authorization: `Bearer ${userHook.user.token}`
+    async function onFormSubmitHandler(e) {
+        e.preventDefault()
+        setErrorMessage('')
+        try {
+            const res = await artShopBackendAxios.put('/profile', {data: formInput ,id: props.id,}, 
+            {
+                headers: {
+                    authorization: `Bearer ${userHook.user.token}`
+                }
+            }
+            )
+            if (res.status === 200) {
+                console.log(formInput)
+                props.setupdated(true)
+                toggle()      
+            }
+        } catch (err) {
+            if (err.response.status === 400 && err.response.data.message === 'Failed to update') {
+                setErrorMessage('Failed to update')
+            }
         }
     }
-      )
-      if (res.status === 200) {
-        console.log(formInput)
-        navigate('/profile')
-          props.setupdated (true)
-          toggle()      
-      }
-  } catch (err) {
-      if (err.response.status === 400 && err.response.data.message === 'Failed to update') {
-          setErrorMessage('Failed to update')
-      }
-  }
-}
 
     return (
      <div>
@@ -82,50 +77,43 @@ async function onFormSubmitHandler(e) {
           Edit Profile
         </Button>
         <Modal isOpen={modal} toggle={toggle} {...props}>
-          <ModalHeader toggle={toggle}>{props.username}</ModalHeader>
+          <ModalHeader toggle={toggle}>Edit profile</ModalHeader>
           <ModalBody>
            <Container>
-           <Row className="mt-3 text-center">
-                <Col>
-                    <h1>Edit Profile</h1>
-                </Col>
-            </Row>
           <Row className="mt-3 justify-content-center">
-                <Col xs={6} className="text-center justify-content-center">
+                <Col className="text-center justify-content-center">
                     <Form className="border border-dark-subtle pt-5 pb-4 ps-4 pe-4" onSubmit={onFormSubmitHandler}>
-                        <div className="d-flex justify-content-center mb-3">
-                        </div>
                         {errorMessage !== '' 
                         ? <div>
                             <p className="text-danger fw-bold">{errorMessage}</p>
                         </div>
                         : ''}
-                        <Form.Group className="mb-3" controlId="formUsername">
-                            <Form.Control type="text" placeholder={props.username} name="username" required onChange={onFormChangeHandler}/>
-                        </Form.Group>
+                        {/* <Form.Group className="mb-3" controlId="formUsername">
+                            <Form.Control type="text" value={formInput.username} name="username" required onChange={onFormChangeHandler}/>
+                        </Form.Group> */}
                         <Form.Group className="mb-3" controlId="formFullName">
-                            <Form.Control type="text" placeholder={props.fullName} name="fullName" required onChange={onFormChangeHandler}/>
+                            <Form.Control type="text" value={formInput.fullName} name="fullName" required onChange={onFormChangeHandler}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formEmail">
-                            <Form.Control type="email" placeholder={props.email} name="email" required onChange={onFormChangeHandler}/>
+                            <Form.Control type="email" value={formInput.email} name="email" required onChange={onFormChangeHandler}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formContactNo">
-                            <Form.Control type="phone" placeholder={props.contactNo} name="contactNo" required onChange={onFormChangeHandler}/>
+                            <Form.Control type="phone" value={formInput.contactNo} name="contactNo" required onChange={onFormChangeHandler}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formAddress">
-                            <Form.Control type="text" placeholder={props.address} name="address" required onChange={onFormChangeHandler}/>
+                            <Form.Control type="text" value={formInput.address} name="address" required onChange={onFormChangeHandler}/>
                         </Form.Group>
                         <div className="mb-3 d-flex justify-content-start">
                             <div className="me-3">
-                                <Button variant="secondary" type="button" onClick={() => widgetRef.current.open()}>Upload Art Image</Button>
+                                <Button variant="secondary" type="button" onClick={() => widgetRef.current.open()}>Upload profile image</Button>
                             </div>
                             
-                            {formInput?.artImage && <div>
-                                    <img src={formInput.artImage} style={{width: 200, height: 200}} alt={'Art Img'}/>
+                            {formInput?.profilePic && <div>
+                                    <img src={formInput.profilePic} style={{width: 200, height: 200}} alt={'Profile Img'}/>
                                 </div>}
                         </div>
                         <Button variant="primary" type="submit" className="fw-medium mb-3">
-                            Edit Profile
+                            Save
                         </Button>
                     </Form>
                 </Col>
